f425a175b4a3ef59782154122f8866263c376134
web@Artmes37 <salamancat592@gmail.com>
2025-11-13 06:22:31 +0300

    Add files via upload

---
+++ black.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
+print(results.prettify())
+
+job_cards = results.find_all("div", class_="card-content")
+for job_card in job_cards:
+     title_element = job_card.find("h2", class_="title")
+     company_element = job_card.find("h3", class_="company")
+     location_element = job_card.find("p", class_="location")
+     print(title_element.text.strip())
+     print(company_element.text.strip())
+     print(location_element.text.strip())
+     print()
+
